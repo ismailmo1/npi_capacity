09f3d0c84742f4a0c9a7be68e630b8523af40ba9
Ismail Mohammed <ismailmo4@gmail.com>
2021-10-17 15:58:25 +0100

    refactor orderbook analysis

--- app.py
+++ app.py
@@ -3,29 +3,35 @@ from flask import Flask, request
 from flask.templating import render_template
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
+from wtforms.fields.core import IntegerField
 
 app = Flask(__name__)
 app.debug = True
 app.secret_key = "secret"
 
 
-class UploadFileForm(FlaskForm):
+class CapacityAnalysisForm(FlaskForm):
     sql_xl = FileField()
     work_to = FileField()
     orderbook = FileField()
+    mould_setup_mins = IntegerField(default=90)
+    pre_mould_days = IntegerField(default=2)
+    post_mould_days = IntegerField(default=9)
+    doc_days = IntegerField(default=4)
+    total_shift_mins = IntegerField(default=360)
 
 
 @app.route("/", methods=["GET", "POST"])
 def home():
-    form = UploadFileForm()
+    cap_analysis_form = CapacityAnalysisForm()
     table = None
-    if form.validate_on_submit():
+    if cap_analysis_form.validate_on_submit():
         sql_data = request.files["sql_xl"].read()
         df = pd.read_excel(sql_data, header=8)
         table = df.to_html(
             classes=["table", "table-hover", "table-responsive"], border=0
         )
-    return render_template("index.html", form=form, table=table)
+    return render_template("index.html", form=cap_analysis_form, table=table)
 
 
 if __name__ == "__main__":
--- npi_capacity.py
+++ npi_capacity.py
@@ -8,9 +8,13 @@ import numpy as np
 import pandas as pd
 from pandas.tseries.offsets import BDay
 
+mould_setup_mins = 90
+pre_mould_days = 2
+post_mould_days = 9
+doc_days = 4
+total_shift_mins = 360
+
 ## read in excel reports
-
-
 wtl_df = pd.read_excel("TSS_ShopFloorWorkToList.xls", header=8)
 ordrbk_df = pd.read_excel(
     "TSS_Assembly_Silastics_MachinedSeals_Report.xls", header=0
@@ -36,50 +40,6 @@ print(
 df_sql = pd.read_excel("npi_capacity_refresh.xlsm", engine="openpyxl").dropna(
     how="all", axis=1
 )
-# con = pyodbc.connect('Driver={SQL Server};'
-#                       'Server=tssstrfsh006;'
-#                       'Database=fsdbtt;'
-#                       'Trusted_Connection=yes;')
-
-# tool_qry = f'''SELECT TOP 200 Bills.ComponentItemNumber,
-# Bills.RequiredQuantity,
-# Bills.ParentItemKey,
-# Bills.ComponentItemKey,
-# Items.ItemNumber As ParentItemNum,
-# Items.ItemDescription As ParentItemDesc,
-# Items2.ItemDescription as CompDesc
-
-# FROM
-# 	FS_BillOfMaterial Bills JOIN FS_Item Items
-# 		ON Bills.ParentItemKey = Items.ItemKey
-# 	JOIN FS_Item Items2
-# 		ON Bills.ComponentItemKey = Items2.ItemKey
-
-# WHERE (Bills.ComponentItemNumber IN {item_list}) AND (Items.ItemNumber LIKE '___-___-%') AND
-# (OR Bills.ComponentItemNumber LIKE 'WC[[]R]___-___%'
-# OR Bills.ComponentItemNumber LIKE 'WC[[]R]%Y[0-9]%')'''
-
-# tool_qry = f'''SELECT TOP 200 Bills.ComponentItemNumber,
-# Bills.RequiredQuantity,
-# Bills.ParentItemKey,
-# Bills.ComponentItemKey,
-# Items.ItemNumber As ParentItemNum,
-# Items.ItemDescription As ParentItemDesc,
-# Items2.ItemDescription as CompDesc
-
-# FROM
-# 	FS_BillOfMaterial Bills JOIN FS_Item Items
-# 		ON Bills.ParentItemKey = Items.ItemKey
-# 	JOIN FS_Item Items2
-# 		ON Bills.ComponentItemKey = Items2.ItemKey
-
-# WHERE (Bills.ComponentItemNumber IN {item_list}) AND (Items.ItemNumber LIKE '___-___-%') AND
-# (Bills.ComponentItemNumber LIKE 'WC[[]R]MOULD%'
-# OR Bills.ComponentItemNumber LIKE 'WC[[]R]1010%'
-# OR Bills.ComponentItemNumber LIKE 'WC[[]R]1012%')'''
-
-# df_tool = pd.read_sql(tool_qry, con)
-# df_mould = pd.read_sql(mould_qry, con)
 
 
 def match_tool_desc(rw):
@@ -88,93 +48,98 @@ def match_tool_desc(rw):
 
 
 tool_mask = df_sql.apply(lambda x: match_tool_desc(x), axis=1)
-
-df_tool = df_sql.loc[tool_mask]
 df_mould = df_sql.loc[~tool_mask]
 
-# clean tool desc to get cavs out (reusing from alternate tool finder)
 
-# regex looks mad complicated but use  pyregex.com to help test patterns
-# use findall to return list of matches
-df_tool.loc[:, "CAVS"] = df_tool.loc[:, "CompDesc"].apply(
-    lambda x: re.findall("[,]?\s?[S/.0-9]{1,3}\s*(?:CAV[S]?|C|INS)[,]?", x)
-)
+def get_tool_cavs(df_sql: pd.DataFrame):
+    tool_mask = df_sql.apply(lambda x: match_tool_desc(x), axis=1)
+    df_tool = df_sql.loc[tool_mask]
 
-# function to extract numbers from cavs list
-def getcavs(cavlist):
+    # clean tool desc to get cavs out (reusing from alternate tool finder)
 
-    # create mini func to convert stuff to numbers and sort out single cavs
-    def cav_to_int(cavstr):
-        return int(re.sub("S[/.]?", "1", cavstr))
+    # regex looks mad complicated but use  pyregex.com to help test patterns
+    # use findall to return list of matches
+    df_tool.loc[:, "CAVS"] = df_tool.loc[:, "CompDesc"].apply(
+        lambda x: re.findall("[,]?\s?[S/.0-9]{1,3}\s*(?:CAV[S]?|C|INS)[,]?", x)
+    )
 
-    num = []
-    # loop through list of numbers to find numbers or 'S/',
-    # this will look for first match and return it, otherwise no return
-    for x in cavlist:
-        m = re.search("S[/.]|[0-9]{1,3}", x)
-        if m:
-            num.append(m.group(0))
+    # function to extract numbers from cavs list
+    def getcavs(cavlist):
 
-    # if theres only one match then return that
-    if len(num) == 1:
-        cavs = cav_to_int(num[0])
-        # assume 200 cavs is incorrect detection
-        if cavs > 200:
-            cavs = 1
+        # create mini func to convert stuff to numbers and sort out single cavs
+        def cav_to_int(cavstr):
+            return int(re.sub("S[/.]?", "1", cavstr))
 
-        return cavs
-    # if theres two matches..
-    elif len(num) == 2:
-        # and both are the same, add the first element of that list together and return it
-        if num[0] == num[1]:
-            return sum((cav_to_int(num[0]), cav_to_int(num[1])))
-        # if theyre not the same then return the full list (need to convert to number to use max())
+        num = []
+        # loop through list of numbers to find numbers or 'S/',
+        # this will look for first match and return it, otherwise no return
+        for x in cavlist:
+            m = re.search("S[/.]|[0-9]{1,3}", x)
+            if m:
+                num.append(m.group(0))
+
+        # if theres only one match then return that
+        if len(num) == 1:
+            cavs = cav_to_int(num[0])
+            # assume 200 cavs is incorrect detection
+            if cavs > 200:
+                cavs = 1
+
+            return cavs
+        # if theres two matches..
+        elif len(num) == 2:
+            # and both are the same, add the first element of that list together and return it
+            if num[0] == num[1]:
+                return sum((cav_to_int(num[0]), cav_to_int(num[1])))
+            # if theyre not the same then return the full list (need to convert to number to use max())
+            else:
+                return max((cav_to_int(num[0]), cav_to_int(num[1])))
         else:
-            return max((cav_to_int(num[0]), cav_to_int(num[1])))
-    else:
-        # return 1 cav as fallback
-        return 1
+            # return 1 cav as fallback
+            return 1
+
+    df_tool.loc[:, "ToolCavs"] = df_tool.loc[:, "CAVS"].apply(getcavs)
+
+    # get rid of initial cav column
+    df_tool.drop("CAVS", axis=1, inplace=True)
+
+    df_tool = df_tool.loc[:, ("ParentItemNum", "ToolCavs")]
+
+    return df_tool
 
 
-df_tool.loc[:, "ToolCavs"] = df_tool.loc[:, "CAVS"].apply(getcavs)
+def get_cycle_times(df_tool: pd.DataFrame, df_mould: pd.DataFrame):
+    df_mould.loc[:, "MouldTimeMin"] = df_mould.loc[:, "RequiredQuantity"] * 60
 
-# get rid of initial cav column
-df_tool.drop("CAVS", axis=1, inplace=True)
+    df_mould = df_mould.loc[:, ("ParentItemNum", "MouldTimeMin")]
 
-df_tool = df_tool.loc[:, ("ParentItemNum", "ToolCavs")]
+    df_cycle_times = df_tool.merge(
+        df_mould, on="ParentItemNum", how="outer", indicator=True
+    )
 
-df_mould.loc[:, "MouldTimeMin"] = df_mould.loc[:, "RequiredQuantity"] * 60
+    def findCycleTime(rw):
+        """use finalCavs to find cycle time - threshold: 15min - 5min(we're trying to guess what the accountants used as the basis for mould time calc)"""
+        try:
+            cycle_time = round(rw["ToolCavs"] * rw["MouldTimeMin"])
+        except ValueError:
+            return None
+        if cycle_time <= 5:
+            return 5
+        elif cycle_time > 15:
+            return 15
+        return cycle_time
 
-df_mould = df_mould.loc[:, ("ParentItemNum", "MouldTimeMin")]
+    df_cycle_times["ExpectedCycleTime"] = df_cycle_times.apply(
+        lambda x: findCycleTime(x), axis=1
+    )
 
-df_cycle_times = df_tool.merge(
-    df_mould, on="ParentItemNum", how="outer", indicator=True
-)
+    df_cycle_times = df_cycle_times.loc[
+        :, ("ParentItemNum", "ToolCavs", "ExpectedCycleTime")
+    ]
 
+    df_cycle_times = pd.read_csv("mock_cycle_times.csv")
 
-def findCycleTime(rw):
-    """use finalCavs to find cycle time - threshold: 15min - 5min(we're trying to guess what the accountants used as the basis for mould time calc)"""
-    try:
-        cycle_time = round(rw["ToolCavs"] * rw["MouldTimeMin"])
-    except ValueError:
-        return None
-    if cycle_time <= 5:
-        return 5
-    elif cycle_time > 15:
-        return 15
-    return cycle_time
-
-
-df_cycle_times["ExpectedCycleTime"] = df_cycle_times.apply(
-    lambda x: findCycleTime(x), axis=1
-)
-
-df_cycle_times = df_cycle_times.loc[
-    :, ("ParentItemNum", "ToolCavs", "ExpectedCycleTime")
-]
-
-
-df_cycle_times = pd.read_csv("mock_cycle_times.csv")
+    return df_cycle_times
 
 
 # ## get current stock levels
@@ -231,28 +196,31 @@ ordrbk_df["req_week"] = ordrbk_df["Req Ship"].apply(lambda x: x.week)
 # # add document requirements (3.1, FAIR etc)
 
 
-def detect_quality_docs(
-    ext_text: str,
-    keywords: list = ["PSQ", "3.1", "FAIR", "ISIR", "LICENSE", "LICENCE"],
-) -> str:
-    """detects presence of keywords in ext_text"""
-    if type(ext_text) == str:
-        for word in keywords:
-            if word in ext_text.upper():
-                return word
-    return None
+def add_quality_days(ordrbk_df: pd.DataFrame):
+    def detect_quality_docs(
+        ext_text: str,
+        keywords: list = ["PSQ", "3.1", "FAIR", "ISIR", "LICENSE", "LICENCE"],
+    ) -> str:
+        """detects presence of keywords in ext_text"""
+        if type(ext_text) == str:
+            for word in keywords:
+                if word in ext_text.upper():
+                    return word
+        return None
 
+    ordrbk_df["extra_docs"] = ordrbk_df["COLExt Text"].apply(
+        detect_quality_docs
+    )
+    ordrbk_df["extra_docs_days"] = ordrbk_df["extra_docs"].apply(
+        lambda x: 4 if x else 0
+    )
 
-ordrbk_df["extra_docs"] = ordrbk_df["COLExt Text"].apply(detect_quality_docs)
-ordrbk_df["extra_docs_days"] = ordrbk_df["extra_docs"].apply(
-    lambda x: 4 if x else 0
-)
+    return ordrbk_df
 
 
 # ## calculate demand based off stock and CO qty
 
 
-# curr_stock_df = ordrbk
 curr_stock_df = ordrbk_df.dropna(subset=["curr_stock"])[
     ["COItem", "curr_stock"]
 ]
@@ -263,7 +231,6 @@ ordrbk_stock_df = ordrbk_df[
 ordrbk_stock_df["cumsum"] = ordrbk_stock_df.groupby(["COItem"])[
     "COQty"
 ].transform(pd.Series.cumsum)
-# ordrbk_stock_df['curr_stock'] = ordrbk_stock_df.groupby(['COItem'])['curr_stock'].bfill()
 
 curr_stock_df.drop_duplicates(subset="COItem", inplace=True)
 
@@ -301,12 +268,6 @@ ordrbk_stock_df = ordrbk_stock_df[
     ]
 ]
 
-
-mould_setup_mins = 90
-pre_mould_days = 2
-post_mould_days = 9
-doc_days = 4
-total_shift_mins = 360
 
 lead_time_df = ordrbk_stock_df.merge(
     df_cycle_times, left_on="COItem", right_on="ParentItemNum", how="left"
